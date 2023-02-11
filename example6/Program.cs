efcbf7cb94d218b1727f9df79db9195f8d8f0919
Anna Bolotova <anechkamilaha69@gmail.com>
2023-02-11 11:00:48 +0300

    создали программу привествия

---
+++ example6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите ваше имя");
+string username = Console.ReadLine();
+
+if (username == "Anna")
+{
+    Console.Write("Привет, крошка ");
+    Console.Write(username);
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
